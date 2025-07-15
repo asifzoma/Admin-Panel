f96716854a72c149f06e065c8ba9a11d40589732
Asif Ahmad <asif.zoma.ahmad@gmail.com>
2025-07-15 15:46:10 +0100

    moved files as overly nested
    moved files as overly nested project was within a folder, within a folder, within a folder, so un nested the projected by moving files.
    
    Database recreated and reseeded.

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -19,4 +19,4 @@ if (PHP_VERSION_ID < 50600) {
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit65fdaf02cf2a4dc9241e927f36d36b8e::getLoader();
+return ComposerAutoloaderInit88970a0117c062eed55fa8728fc43833::getLoader();
