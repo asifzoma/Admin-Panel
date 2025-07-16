b8a7ab10fa684aeb6b0d970a19b81c31c391f304
Asif Ahmad <asif.zoma.ahmad@gmail.com>
2025-07-16 15:29:40 +0100

    Activity log update
    Activity log update

--- app/Models/ActivityLog.php
+++ app/Models/ActivityLog.php
@@ -6,6 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class ActivityLog extends Model
 {
+    protected $fillable = [
+        'user_id',
+        'action',
+        'subject_type',
+        'subject_id',
+        'description',
+    ];
+
     public function user()
     {
         return $this->belongsTo(\App\Models\User::class);
